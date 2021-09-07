0a5f92542bc8f34a1d5d08d834e18ae5166c8aae
jasmine <jche0155@student.monash.edu>
2021-09-07 20:25:41 +0800

    updated workable edit function

--- script.js
+++ script.js
@@ -7,19 +7,21 @@ request.onreadystatechange = function(){
     listAll(datalist);
   }
 }
-request.open('GET', 'https://gorest.co.in/public/v1/users?page=1', true);
+request.open('GET', 'https://gorest.co.in/public/v1/users?page=76', true);
 request.setRequestHeader("Content-type", "application/json");
 request.setRequestHeader("Authorization", "Bearer d65c1eb53080a7e1585ec8734451807790d83c28532025a568ef7c9d03bb29d8");
 request.send();
 
 function listAll(datalist){
   for(i in datalist.data){
-    document.getElementById("displaytable").innerHTML += "<tr id='row" + i + "' contenteditable=\"false\"><td id='name" + i + "'>" + datalist.data[i].name +
-    "</td><td id='email" + i + "'>" + datalist.data[i].email + "</td><td id='gender" + i + "'>" + datalist.data[i].gender + "</td><td id='status" + i + "'>" + datalist.data[i].status +
-    "</td><td><button class='mdl-button mdl-js-button mdl-button--icon' onclick=\"deleteUser(" + datalist.data[i].id + ")\"><i class='material-icons'>delete</i></button></td>"+
-    "<td><button class='mdl-button mdl-js-button mdl-button--icon' id=\"editbtn" + i + "\"onclick=\"enableEdit("+ i + ")\"><i class='material-icons'>edit</i></button></td>"+
-    "<td><button class='mdl-button mdl-js-button' id=\"savebtn" + i + "\" onclick=\"saveEdit("+ i + "," + datalist.data[i].id + ")\">save</button></td>"+
-    "<td><button class='mdl-button mdl-js-button' id=\"cancelbtn" + i + "\" onclick=\"cancelEdit("+ i + ")\">cancel</button></td></tr>";
+    if(datalist.data[i].id !== "undefined"){
+      document.getElementById("displaytable").innerHTML += "<tr id='row" + i + "' contenteditable=\"false\"><td id='name" + i + "'>" + datalist.data[i].name +
+      "</td><td id='email" + i + "'>" + datalist.data[i].email + "</td><td id='gender" + i + "'>" + datalist.data[i].gender + "</td><td id='status" + i + "'>" + datalist.data[i].status +
+      "</td><td><button class='mdl-button mdl-js-button mdl-button--icon' onclick=\"deleteUser(" + datalist.data[i].id + ")\"><i class='material-icons'>delete</i></button></td>"+
+      "<td><button class='mdl-button mdl-js-button mdl-button--icon' id=\"editbtn" + i + "\"onclick=\"enableEdit("+ i + ")\"><i class='material-icons'>edit</i></button></td>"+
+      "<td><button class='mdl-button mdl-js-button' id=\"savebtn" + i + "\" onclick=\"saveEdit("+ i + "," + datalist.data[i].id + ")\">save</button></td>"+
+      "<td><button class='mdl-button mdl-js-button' id=\"cancelbtn" + i + "\" onclick=\"cancelEdit("+ i + ")\">cancel</button></td></tr>";
+    }
   }
 }
 
@@ -79,14 +81,11 @@ function saveEdit(currRow, currUserId){
   document.getElementById("savebtn"+currRow).style.visibility = "hidden"
   document.getElementById("cancelbtn"+currRow).style.visibility = "hidden"
 
-  let name = document.getElementById("name"+currRow).value;
-  let email = document.getElementById("email"+currRow).value;
-  let gender = document.getElementById("gender"+currRow).value;
-  let status = document.getElementById("status"+currRow).value;
+  let name = document.getElementById("name"+currRow).innerText;
+  let email = document.getElementById("email"+currRow).innerText;
+  let gender = document.getElementById("gender"+currRow).innerText;
+  let status = document.getElementById("status"+currRow).innerText;
 
-  console.log(currRow)
-  console.log(currUserId)
-  console.log(document.getElementById("status"+currRow).value)
   request.open('PUT', 'https://gorest.co.in/public/v1/users/' + currUserId, true);
   request.setRequestHeader("Content-type", "application/json");
   request.setRequestHeader("Authorization", "Bearer d65c1eb53080a7e1585ec8734451807790d83c28532025a568ef7c9d03bb29d8");
@@ -96,7 +95,7 @@ function saveEdit(currRow, currUserId){
   }
 }
 
-function nextPage(){
+function nextPage(nextPgae){
   page+=1;
   read(page);
 }
